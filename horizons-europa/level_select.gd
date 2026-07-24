8cef2987c14989ea95af037a8e074f4c62134295
Annelies <118388665+Starianne@users.noreply.github.com>
2026-07-24 20:44:36 +0200

    menu + level setup screen

---
+++ horizons-europa/level_select.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://level1.tscn")
---
+++ horizons-europa/main_menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_select.tscn")
+
+
+func _on_options_pressed() -> void:
+	pass 
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
