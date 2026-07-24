e3252a8b9e7cd7911c32a7428e8510b845a7ee21
palebrownbutton <simonavatcheva@hotmail.com>
2026-07-24 19:45:20 +0100

    Update monster.gd

--- horizons-europa/monster.gd
+++ horizons-europa/monster.gd
@@ -9,17 +9,19 @@ var lane2 = 282
 var lane3 = 411
 var lane4 = 540
 
-
 func _ready():
 	var lanes = [lane1, lane2, lane3, lane4]
 	position.y = lanes.pick_random()
 	start_position = position
 
-
 func _process(delta):
 	position.x -= speed * delta
-	
-	if position.x <= -size.x:
-		position.x = start_position.x
+	if global_position.x <= $"../Node2D".global_position.x:
+		visible = false
 		
+		await get_tree().create_timer(1.0).timeout
+		
+		position = start_position
 		position.y = [lane1, lane2, lane3, lane4].pick_random()
+		
+		visible = true
