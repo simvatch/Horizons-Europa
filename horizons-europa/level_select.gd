d88a82f129baf01b83cff969471a393bd506f85a
Annelies <118388665+Starianne@users.noreply.github.com>
2026-07-25 13:22:41 +0200

    added level 2 btn

--- horizons-europa/level_select.gd
+++ horizons-europa/level_select.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://level1.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://level1.tscn")
