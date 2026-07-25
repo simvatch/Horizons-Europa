e3086fe70e4d82273c33c227ee8509839c445164
Annelies <118388665+Starianne@users.noreply.github.com>
2026-07-25 13:24:23 +0200

    fixed level select

--- horizons-europa/level_select.gd
+++ horizons-europa/level_select.gd
@@ -7,4 +7,4 @@ func _on_level_1_pressed() -> void:
 
 
 func _on_level_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://level1.tscn")
+	get_tree().change_scene_to_file("res://level2.tscn")
